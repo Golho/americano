7ade9fc9015f544fba15791b6fcffdead1e3e174
Olov <olov.gunther@gmail.com>
2026-04-13 21:52:27 +0200

    Add build timestamp to dist/index.html file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
+const buildTimestamp = new Date().toISOString();
+
 export default defineConfig({
-  plugins: [viteSingleFile()],
+  plugins: [
+    viteSingleFile(),
+    {
+      name: 'html-build-time',
+      transformIndexHtml: (html) => html.replace('__BUILD_TIME__', buildTimestamp),
+    },
+  ],
   test: {
     environment: 'node',
   },
